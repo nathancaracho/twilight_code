92f12722e8de862b5f53e73d96cfdc1430f5f4c2
‘Nathan <nathan.caracho@hotmail.com>
2025-07-22 00:08:32 -0300

    fix deploy action

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,8 @@ import catppuccin from "@catppuccin/starlight";
 import astroD2 from 'astro-d2'
 
 export default defineConfig({
+  site: 'https://nathancaracho.github.io',
+  base: '/twilight_code',
   integrations: [starlight({
     title: 'Twilight code',
     social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nathancaracho/twilight_code' }],
